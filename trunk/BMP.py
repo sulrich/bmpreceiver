32193723497ea668dc2da28af969babc901e56f1
stephen.stuart@gmail.com <stephen.stuart@gmail.com@7d508e1c-821b-11de-a106-ebce2304a7ed>
2014-06-03 16:46:11 +0000

    Updated to understand BMP version 3, thanks to a test file provided by folks from Cisco.

--- trunk/BMP.py
+++ trunk/BMP.py
@@ -25,22 +25,48 @@ import struct
 import time
 import indent
 
+# The length of the version field that starts every BMP message.
+#
+VERSION_LEN = 1
+
 # The length of the fixed header part of a BMP message.
 #
-HEADER_LEN = 44
+HEADER_LEN_V1 = 44
+HEADER_LEN_V3 = 6
+PER_PEER_HEADER_LEN_V3 = 42
+
+# various message lengths
+#
+PEER_UP_LEN = 20
 
 # Version of the protocol, as specified in the header.
 #
-VERSION = 1
+VERSION_MIN = 1
+VERSION_MAX = 3
 
 # Message types.
 #
 MSG_TYPE_ROUTE_MONITORING = 0
 MSG_TYPE_STATISTICS_REPORT = 1
 MSG_TYPE_PEER_DOWN_NOTIFICATION = 2
+MSG_TYPE_PEER_UP_NOTIFICATION = 3
+MSG_TYPE_INITIATION_MESSAGE = 4
+MSG_TYPE_TERMINATION_MESSAGE = 5
 MSG_TYPE_STR = {MSG_TYPE_ROUTE_MONITORING: "Route Monitoring",
                 MSG_TYPE_STATISTICS_REPORT: "Statistics Report",
-                MSG_TYPE_PEER_DOWN_NOTIFICATION: "Peer Down Notification"}
+                MSG_TYPE_PEER_DOWN_NOTIFICATION: "Peer Down Notification",
+                MSG_TYPE_PEER_UP_NOTIFICATION: "Peer Up Notification",
+                MSG_TYPE_INITIATION_MESSAGE: "Initiation Message",
+                MSG_TYPE_TERMINATION_MESSAGE: "Termination Message"}
+
+# Initiation message information types
+#
+INIT_INFO_TYPE_STRING = 0
+INIT_INFO_TYPE_SYSDESCR = 1
+INIT_INFO_TYPE_SYSNAME = 2
+INIT_INFO_TYPE_STR = {INIT_INFO_TYPE_STRING: "String",
+                      INIT_INFO_TYPE_SYSDESCR: "sysDescr",
+                      INIT_INFO_TYPE_SYSNAME: "sysName"}
 
 # Peer types.
 #
@@ -69,8 +95,8 @@ PEER_DOWN_REASON_STR = {1: "Local system closed session, notification sent",
                         4: "Remote system closed session, no notification"}
 
 
-def ParseBmpHeader(header, verbose=False):
-  """Parse a BMP header.
+def ParseBmpHeaderV1(header, verbose=False):
+  """Parse a BMP V1 header.
 
   Args:
     header: array containing BMP message header.
@@ -87,20 +113,20 @@ def ParseBmpHeader(header, verbose=False):
   indent_str = indent.IndentLevel(indent.BMP_HEADER_INDENT)
   print_msg = []
 
-  version, msg_type, peer_type, peer_flags = struct.unpack(">BBBB",
-                                                           header[0:4])
+  version = 1
+  msg_type, peer_type, peer_flags = struct.unpack(">BBB",
+                                                  header[0:3])
   if peer_flags & PEER_FLAG_IPV6:
-    peer_address = socket.inet_ntop(socket.AF_INET6, header[12:28])
+    peer_address = socket.inet_ntop(socket.AF_INET6, header[11:27])
   else:
-    peer_address = socket.inet_ntop(socket.AF_INET, header[24:28])
+    peer_address = socket.inet_ntop(socket.AF_INET, header[23:27])
   peer_as, time_sec = struct.unpack(">LxxxxL",
-                                    header[28:40])
+                                    header[27:39])
 
   # If we have a version mismatch, we're pretty much done here.
   #
-  if version != VERSION:
-    raise ValueError("Found BMP version %d, expecting %d" % (version,
-                                                             VERSION))
+  if version != 1:
+    raise ValueError("Found BMP version %d, expecting %d" % (version, 1))
 
   # Decide what to format as text
   #
@@ -114,13 +140,135 @@ def ParseBmpHeader(header, verbose=False):
     print_msg.append("%speer_type %s" % (indent_str,
                                          PEER_TYPE_STR[peer_type]))
     print_msg.append(" peer_flags 0x%x" % peer_flags)
-    print_msg.append(" router_id %s\n" % socket.inet_ntoa(header[32:36]))
+    print_msg.append(" router_id %s\n" % socket.inet_ntoa(header[31:34]))
     print_msg.append("%stime %s\n" % (indent_str, time.ctime(time_sec)))
 
   # Return the message type so the caller can decide what to do next,
   # and the list of strings representing the collected message.
   #
   return msg_type, print_msg
+
+
+def ParseBmpHeaderV3(header, verbose=False):
+  """Parse a BMP V3 header.
+
+  Args:
+    header: array containing BMP message header.
+    verbose: be chatty, or not.
+
+  Returns:
+    An int indicating the type of message that follows the header,
+    and a list of strings to print.
+
+  Raises:
+    ValueError: an unexpected value was found in the message
+  """
+
+  indent_str = indent.IndentLevel(indent.BMP_HEADER_INDENT)
+  print_msg = []
+
+  version = 3
+  msg_length = struct.unpack(">L", header[0:4])
+  msg_type = header[4];
+
+  # Decide what to format as text
+  #
+  print_msg.append("%sBMP version %d type %s length %d\n" %
+                   (indent_str,
+                    version,
+                    MSG_TYPE_STR[msg_type],
+                    msg_length[0]))
+
+  # Return the message type so the caller can decide what to do next,
+  # and the list of strings representing the collected message.
+  #
+  return msg_type, msg_length[0], print_msg
+
+
+def ParseBmpPeerUp(message, peer_flags, verbose=False):
+  """Parse a BMP V3 Peer Up message.
+
+  Args:
+    header: array containing BMP peer up message.
+    peer_flags: from the per-peer header.
+    verbose: be chatty, or not.
+
+  Returns:
+    A list of strings to print.
+
+  Raises:
+    ValueError: an unexpected value was found in the message
+  """
+
+  indent_str = indent.IndentLevel(indent.BMP_CONTENT_INDENT)
+  print_msg = []
+  offset = 0
+
+  if peer_flags & PEER_FLAG_IPV6:
+    loc_addr = socket.inet_ntop(socket.AF_INET6, 
+                                message[offset : offset + 16])
+  else:
+    loc_addr = socket.inet_ntop(socket.AF_INET, 
+                                message[offset + 12 : offset + 16])
+  offset += 16
+  loc_port, rem_port = struct.unpack_from(">HH", message, offset)
+
+  print_msg.append("%sloc_addr %s, loc_port %d, rem_port %d\n" % (indent_str,
+                                                                  loc_addr,
+                                                                  loc_port,
+                                                                  rem_port))
+  return print_msg
+
+
+def ParseBmpPerPeerHeaderV3(header, verbose=False):
+  """Parse a BMP V3 per-peer header.
+
+  Args:
+    header: array containing BMP message header.
+    verbose: be chatty, or not.
+
+  Returns:
+    An int indicating the peer flags
+    and a list of strings to print.
+
+  Raises:
+    ValueError: an unexpected value was found in the message
+  """
+
+  indent_str = indent.IndentLevel(indent.BMP_CONTENT_INDENT)
+  print_msg = []
+  offset = 0
+
+  # peer type, flags, and the rest
+  #
+  peer_type, peer_flags = struct.unpack_from(">BB", header, offset)
+  offset += 2
+  peer_dist = struct.unpack_from("xxxxxxxx", header, offset)
+  offset += 8
+  if peer_flags & PEER_FLAG_IPV6:
+    peer_address = socket.inet_ntop(socket.AF_INET6, 
+                                    header[offset : offset + 16])
+  else:
+    peer_address = socket.inet_ntop(socket.AF_INET, 
+                                    header[offset + 12 : offset + 16])
+  offset += 16
+  peer_as, peer_bgp_id, time_sec, time_usec = struct.unpack_from(">LLLL",
+                                                                 header,
+                                                                 offset)
+
+  # Decide what to format as text
+  #
+  print_msg.append("%sPeer Type %s, flags %d, address %s, AS %d\n" %
+                   (indent_str,
+                    PEER_TYPE_STR[peer_type],
+                    peer_flags,
+                    peer_address,
+                    peer_as))
+
+  # Return the message type so the caller can decide what to do next,
+  # and the list of strings representing the collected message.
+  #
+  return peer_flags, print_msg
 
 
 # A function indication whether or not a BMP Peer Down message comes
